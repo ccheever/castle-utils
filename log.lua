1cff0aa989233aac27ba8d6d3c31adf5fa2731ba
Charlie Cheever <ccheever@expo.io>
2019-03-12 03:44:01 -0700

    Adding log

---
+++ log.lua
@@ -0,0 +1,24 @@
+--[[
+`log` takes any number of arguments and prints them 
+out as a comma separated list. Anything other than 
+strings will be pretty-printed using serpent.
+]]
+
+local serpent =
+    require(
+    "https://raw.githubusercontent.com/pkulchenko/serpent/522a6239f25997b101c585c0daf6a15b7e37fad9/src/serpent.lua"
+)
+
+local function log(...)
+    local out = {}
+    for i, obj in ipairs({...}) do
+        if type(obj) == "string" then
+            table.insert(out, obj)
+        else
+            table.insert(out, serpent.line(obj))
+        end
+    end
+    print(table.concat(out), ", ")
+end
+
+return log
